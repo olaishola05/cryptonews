743780d416423ddf8b5e0756e11330c6a9e64b15
Ola Ishola <45001916+netman5@users.noreply.github.com>
2021-12-29 21:06:20 +0100

    Add packages for fetching the news

--- controllers/news.js
+++ controllers/news.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const { StatusCodes } = require('http-status-codes');
+const cheerio = require('cheerio');
+const axios = require('axios').default;
+const { v4: uuidv4 } = require('uuid');
 
 const getAllNews = async (req, res) => {
   res.status(StatusCodes.OK).json('im all news');

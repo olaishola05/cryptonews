693b273c9f9601e0e9603295fc633651a7e7b4c1
Ola Ishola <45001916+netman5@users.noreply.github.com>
2021-12-29 22:09:06 +0100

    Create a fetch function and push news info into cryptoNews arry

--- controllers/news.js
+++ controllers/news.js
@@ -4,8 +4,55 @@ const cheerio = require('cheerio');
 const axios = require('axios').default;
 const { v4: uuidv4 } = require('uuid');
 
+const cryptoNews = [];
+
+const newsSources = [
+  {
+    name: 'Time',
+    url: 'https://time.com/nextadvisor/investing/cryptocurrency/latest-crypto-news/',
+  },
+
+  {
+    name: 'Cointelegraph',
+    url: 'https://cointelegraph.com/markets',
+  },
+
+  {
+    name: 'Tradingview',
+    url: 'https://www.tradingview.com/markets/cryptocurrencies/news/',
+  },
+];
+
+function fetchNews() {
+  newsSources.forEach((news) => {
+    axios
+      .get(news.url)
+      .then((response) => {
+        const html = response.data;
+        const $ = cheerio.load(html);
+        $('a:contains("crypto")', html).each(function () {
+          const title = $(this).text();
+          const url = $(this).attr('href');
+
+          cryptoNews.push({
+            id: uuidv4(),
+            title,
+            url,
+            source: news.name,
+          });
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  });
+
+  return cryptoNews;
+}
+fetchNews();
+
 const getAllNews = async (req, res) => {
-  res.status(StatusCodes.OK).json('im all news');
+  res.status(StatusCodes.OK).json({ cryptoNews, nhits: cryptoNews.length });
 };
 
 const getANews = async (req, res) => {
